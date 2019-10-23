ccbe615ba4e1b5bec546c11592e1c9e44de67b97
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-23 20:57:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  def initialize(side_one, side_two, side_three)
+    @side_1 = side_one
+    @side_2 = side_two
+    @side_3 = side_three
+  end
+
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif  (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+     if (@side_1 == @side_2) && (@side_2 == @side_3)
+       :equilateral
+     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+       :isosceles
+     elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+       :scalene
+     end
+   end
+ end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
