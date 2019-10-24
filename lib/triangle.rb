0068b05a54ef9171448e6d8b797b01e9aa75ae08
lmjcbs <hello@liamjacobs.dev>
2019-10-24 21:39:47 +0100

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_reader :len1, :len2, :len3
+
+  def initialize(len1, len2, len3)
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+    @lengths_array = [len1, len2, len3]
+  end
+
+  def test_for_triangle
+    if @lengths_array.all?(Numeric)
+      if @lengths_array.all?{ |len| len > 0 }
+       if (len1 + len2 > len3) && (len2 + len3 > len1) && (len3 + len1 > len2)
+          return true
+       end
+      end
+    end 
+    false
+  end
+
+  def kind
+    if !test_for_triangle
+      raise TriangleError
+    else
+      if len1 == len2 && len2 == len3
+        :equilateral
+      elsif len1 == len2 || len2 == len3 || len1 == len3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
