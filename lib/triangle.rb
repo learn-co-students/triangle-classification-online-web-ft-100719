75fa786b5d8e37fed194672b770ef8b1a4fc228d
Hans Foster <fosterhans@gmail.com>
2019-11-12 01:30:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,20 +13,15 @@ class Triangle
     
     if self.side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a || @side_a <= 0 || @side_b <= 0 || self.side_c <= 0 
       raise TriangleError
-        begin 
-          raise TriangleError
-        rescue TriangleError => error 
-          puts error.message
-        end
-      else 
-        if @side_a == @side_b && @side_b == @side_c
-          return :equilateral
-        elsif @side_a == @side_c || @side_a == @side_b || @side_b == @side_c
-          return :isosceles
-        elsif @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
-          return :scalene
-        end
+    else 
+      if @side_a == @side_b && @side_b == @side_c
+        return :equilateral
+      elsif @side_a == @side_c || @side_a == @side_b || @side_b == @side_c
+        return :isosceles
+      elsif @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
+        return :scalene
       end
+    end
   end
   
   class TriangleError < StandardError
